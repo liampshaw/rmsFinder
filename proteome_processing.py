6755a372cf4ccb9c8204aa0c9be5eb2af4441ba3
liampshaw <liam.philip.shaw@gmail.com>
2021-04-20 16:05:49 +0100

    all functions now have summary

--- proteome_processing.py
+++ proteome_processing.py
@@ -12,12 +12,21 @@ def createMSA(input_fasta, reference_fasta, hmm_file, family, msa_output):
     '''Creates MSA for hit against REBASE sequences from the same family.
 
     Args:
-        protein (dict item)
-            A protein with its sequence
-        '''
-    # get the rebase gold hits with that family out of the fasta file
-    #rebase_hits = extractHitsGold(rebase_gold_fasta, hmm, family)
-    # Load the REBASE hits (prepREBASE)
+        input_fasta (str)
+            Fasta file with protein sequences to align.
+        reference_fasta (str)
+            Fasta file with reference sequences.
+        hmm_file (str)
+            File with hmm profile.
+        family (str)
+            Name of family (within hmm profile).
+        msa_output (str)
+            Filename to write MSA to.
+
+    Returns:
+        1 (success) or 0 (failure)
+    '''
+    # Load the REBASE hits (typically prepared with prepREBASE)
     if os.path.isfile(reference_fasta):
         reference = SeqIO.to_dict(SeqIO.parse(reference_fasta, 'fasta'))
         input = SeqIO.to_dict(SeqIO.parse(input_fasta, 'fasta'))
@@ -55,6 +64,7 @@ def getPID(a, b):
 
 def getTopMatch(query, msa_file, msa_format='stockholm'):
     '''Returns top match (by pairwise similarity) for a protein in a MSA.'''
+    print('Finding match for', query, 'in', msa_file)
     msa = SeqIO.to_dict(SeqIO.parse(msa_file, format=msa_format))
     query_seq = str(msa[query].seq)
     hit_dict = dict()
@@ -65,13 +75,52 @@ def getTopMatch(query, msa_file, msa_format='stockholm'):
     return({k:v for k, v in hit_dict.items() if v==max_value})
 
 
-def hmmer2AlignToReference(protein, reference_msa):
-    '''Aligns a protein to a reference MSA with hmmer2.'''
-    # To be completed
+def hmmer2AlignToReference(input_fasta, reference_msa, hmm_profile, output_aln):
+    '''Aligns an input fasta to a reference MSA with hmmer2.
+
+    Args:
+        input_fasta (str)
+            Filename of fasta file to align.
+        reference_msa (str)
+            Filename of (pre-computed) alignment
+        hmm_profile (str)
+            Filename of hmm profile
+        output_aln (str)
+            Filename of output
+
+    Returns:
+        None
+    '''
+    # To be completed.
+    hmm2align_command = ['/Users/liam/Applications/hmmer-2.3.2/src/hmmalign',
+                        '--withali', reference_msa,
+                        '-o', output_aln,
+                        hmm_profile,
+                        input_fasta]
+    hmm2align_process = subprocess.Popen(hmm2align_command,
+                        stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+    hmm2align_out, _ = hmm2align_process.communicate()
+    #print(hmm2align_out)
     return
 
 def getMSAs(proteome_fasta, hits, hmm, hmm_file, msa_dir):
-    '''Gets all MSAs for a proteome dict.'''
+    '''Gets all MSAs for a proteome dict.
+
+    Args:
+        proteome_fasta (str)
+            Filename of fasta file with proteins in.
+        hits (dict)
+            The best hit for each protein from hmmsearch.
+        hmm (str)
+            The name of the hmm profile.
+        hmm_file (str)
+            Filename of the hmm profile.
+        msa_dir (str)
+            Directory to save output to.
+
+    Returns:
+        None
+    '''
     proteome = SeqIO.to_dict(SeqIO.parse(proteome_fasta, 'fasta'))
     # Take the unique families
     families = list(set([h[1] for h in hits.values()]))
@@ -84,10 +133,14 @@ def getMSAs(proteome_fasta, hits, hmm, hmm_file, msa_dir):
                 if hit[1]==fam:
                     proteins_of_interest.append(prot)
                     f.write('>%s\n%s\n' % (prot, str(proteome[prot].seq)))
-        createMSA(tmp_fasta, msa_dir+'/'+hmm+'.'+fam+'.fa',
-                hmm_file, fam, 'tmp.with.reference.aln')
+        tmp_aln = 'tmp_'+fam+'.aln'
+        reference_aln = msa_dir+'/'+hmm+'.'+fam+'.fa.aln'
+        reference_hmm = msa_dir+'/'+hmm+'.'+fam+'.hmm.2'
+        #print(reference_aln)
+        hmmer2AlignToReference(tmp_fasta, reference_aln,
+                            reference_hmm, tmp_aln)
         for prot in proteins_of_interest:
             print()
             print(prot)
             print(str(proteome[prot].seq))
-            print(getTopMatch(prot, 'tmp.with.reference.aln'))
+            print(getTopMatch(prot, tmp_aln))
