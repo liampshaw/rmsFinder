08cb2960acd8d76f02743718efb2b3488f180ea3
liampshaw <liam.philip.shaw@gmail.com>
2021-04-20 10:58:21 +0100

    building up methods

--- proteome_processing.py
+++ proteome_processing.py
@@ -8,7 +8,7 @@ import os
 import subprocess
 
 # Function to create MSA for each hit with the REBASE Gold standards within that family
-def createMSA(input_fasta, reference_fasta, hmm_file, family):
+def createMSA(input_fasta, reference_fasta, hmm_file, family, msa_output):
     '''Creates MSA for hit against REBASE sequences from the same family.
 
     Args:
@@ -22,25 +22,27 @@ def createMSA(input_fasta, reference_fasta, hmm_file, family):
         reference = SeqIO.to_dict(SeqIO.parse(reference_fasta, 'fasta'))
         input = SeqIO.to_dict(SeqIO.parse(input_fasta, 'fasta'))
         # write these to file
-        tmp_fasta = 'tmp.fa'
+        tmp_fasta = 'tmp.with.reference.'+input_fasta
+        tmp_hmm = 'tmp.hmm'
         with open(tmp_fasta, 'w') as f:
             for r in reference:
                 f.write('>%s\n%s\n' % (r, str(reference[r].seq)))
             for i in input:
                 f.write('>%s\n%s\n' % (i, str(input[i].seq)))
         ##### Fetch just the family of interest and write to tmp.hmm
-        hmm_fetch_command = ['hmmfetch', '-o', 'tmp.hmm', hmm_file, family]
+        hmm_fetch_command = ['hmmfetch', '-o', tmp_hmm, hmm_file, family]
         hmm_fetch_process = subprocess.Popen(hmm_fetch_command, stdout = subprocess.PIPE, stderr = subprocess.PIPE) # Runs the process
         hmm_fetch_out, _ = hmm_fetch_process.communicate() # Read the output from stdout
         #### Compute the MSA
         print('creating MSA...')
-        hmmalign_command = ['hmmalign',  '-o', 'tmp.aln', 'tmp.hmm', 'tmp.fa'] # align, output to tmp.aln
+        hmmalign_command = ['hmmalign',  '-o', msa_output, tmp_hmm, tmp_fasta] # align, output to tmp.aln
         hmmalign_process = subprocess.Popen(hmmalign_command, stdout = subprocess.PIPE, stderr = subprocess.PIPE) # Runs the process
         hmmalign_out, _ = hmmalign_process.communicate() # Read the output from stdout
-        #os.remove(tmp_fasta)
+        os.remove(tmp_fasta)
+        os.remove(tmp_hmm)
         return(1)
     else:
-        print('That family has zero matches in REBASE Gold!')
+        print('That family has zero matches in the reference fasta provided!')
         return(0)
 
 def getPID(a, b):
@@ -51,29 +53,22 @@ def getPID(a, b):
     pid = float(n_identical)/float(min_length) * 100.0
     return(pid)
 
-def getTopMatch(query, msa_file):
+def getTopMatch(query, msa_file, msa_format='stockholm'):
     '''Returns top match (by pairwise similarity) for a protein in a MSA.'''
-    msa = SeqIO.to_dict(SeqIO.parse(msa_file, 'stockholm'))
+    msa = SeqIO.to_dict(SeqIO.parse(msa_file, format=msa_format))
     query_seq = str(msa[query].seq)
     hit_dict = dict()
     for seq in msa.keys():
         if seq!=query:
             hit_dict[seq] = getPID(str(msa[seq].seq), query_seq)
-    return(max(hit_dict.values()))
+    max_value = max(hit_dict.values())
+    return({k:v for k, v in hit_dict.items() if v==max_value})
 
 
-def calculatePID(protein, msa_file):
-    '''Returns the pids (% ids) for a protein in an MSA.'''
-    print('calculating pid...')
-    esl_alipid_command = ['esl-alipid', msa_file] # gets all pids
-    esl_alipid_process = subprocess.Popen(esl_alipid_command, stdout = subprocess.PIPE, stderr = subprocess.PIPE) # Runs the process
-    output = subprocess.check_output(('grep', protein), stdin=esl_alipid_process.stdout) # Find just pairwise comparisons involving protein
-    output_split = output.decode().split('\n') # split output
-    output_df = pd.DataFrame(data=[row.split() for row in output_split]) # make dataframe
-    output_df = output_df.dropna() # remove None/NA
-    output_df[2] = pd.to_numeric(output_df[2]) # convert the pid (col 2) to numeric
-    output_df = output_df.sort_values(2, ascending=False) # sort by decreasing pid
-    return(output_df)
+def hmmer2AlignToReference(protein, reference_msa):
+    '''Aligns a protein to a reference MSA with hmmer2.'''
+    # To be completed
+    return
 
 def getMSAs(proteome_fasta, hits, hmm, hmm_file, msa_dir):
     '''Gets all MSAs for a proteome dict.'''
@@ -83,33 +78,16 @@ def getMSAs(proteome_fasta, hits, hmm, hmm_file, msa_dir):
     print(families)
     for fam in families:
         tmp_fasta = 'tmp_'+fam+'.fa' # Open tmp fasta and write sequences to it
+        proteins_of_interest = []
         with open(tmp_fasta, 'w') as f:
             for prot, hit in hits.items():
                 if hit[1]==fam:
+                    proteins_of_interest.append(prot)
                     f.write('>%s\n%s\n' % (prot, str(proteome[prot].seq)))
         createMSA(tmp_fasta, msa_dir+'/'+hmm+'.'+fam+'.fa',
-                hmm_file, fam)
-
-
-    for prot in hits.keys():
-        fam = hits[prot][1] # family
-        print('')
-        print(prot)
-        print(fam)
-        msa = createMSA(proteome[prot], str(proteome[prot].seq),
-                        msa_dir+'/'+hmm+'.'+fam+'.fa', hmm_file, fam)
-        if msa==0:
-            pass
-        else:
-            output_pid = calculatePID(prot, 'tmp.aln')
-            print(output_pid.iloc[[0]])
-            if 'stored_results' in locals():
-                output_pid['hmm'] = hmm
-                output_pid['family'] = fam
-                stored_results = stored_results.append(output_pid.iloc[[0]])
-                #stored_results['family'][nrow()]
-            else:
-                stored_results = output_pid.iloc[[0]]
-                stored_results['hmm'] = hmm
-                stored_results['family'] = fam
-    return stored_results
+                hmm_file, fam, 'tmp.with.reference.aln')
+        for prot in proteins_of_interest:
+            print()
+            print(prot)
+            print(str(proteome[prot].seq))
+            print(getTopMatch(prot, 'tmp.with.reference.aln'))
